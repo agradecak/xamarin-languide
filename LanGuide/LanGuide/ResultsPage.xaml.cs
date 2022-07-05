2ad43890636b994744fbf019b0700719aa1726a2
Andrija Gradečak <andrija.gradecak@student.uniri.hr>
2022-07-05 15:08:04 +0200

    ispisivanje users i results

--- LanGuide/LanGuide/ResultsPage.xaml.cs
+++ LanGuide/LanGuide/ResultsPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using LanGuide.Models;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +15,38 @@ namespace LanGuide
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ResultsPage : ContentPage
     {
+        List<Result> results = new List<Result>();
         public ResultsPage()
         {
             InitializeComponent();
+            GetAPIdata();
+        }
+
+        public async Task GetAPIdata()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(new Uri("https://www.idt.mdh.se/personal/plt01/languide/?get=results"));
+                var result = await response.Content.ReadAsStringAsync();
+                var json_object = JObject.Parse(result);
+                var json_array = JArray.Parse(json_object["data"].ToString());
+                foreach (var user in json_array)
+                {
+                    Result rslt = new Result();
+                    rslt.id_user = user["id_user"].ToString();
+                    rslt.email = user["email"].ToString();
+                    rslt.create_time = user["create_time"].ToString();
+                    rslt.id_exercise = user["id_exercise"].ToString();
+                    rslt.result_percent = user["result_percent"].ToString();
+                    rslt.result_score = user["result_score"].ToString();
+                    rslt.result_max = user["result_max"].ToString();
+                    rslt.skill = user["skill"].ToString();
+                    rslt.language = user["language"].ToString();
+                    rslt.result_date = user["result_date"].ToString();
+                    results.Add(rslt);
+                }
+            }
+            resultListView.ItemsSource = results;
         }
     }
 }
--- LanGuide/LanGuide/UsersPage.xaml.cs
+++ LanGuide/LanGuide/UsersPage.xaml.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using LanGuide.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +15,31 @@ namespace LanGuide
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class UsersPage : ContentPage
     {
+        List<User> users = new List<User>();
         public UsersPage()
         {
             InitializeComponent();
+            GetAPIdata();
+        }
+
+        public async Task GetAPIdata()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(new Uri("https://www.idt.mdh.se/personal/plt01/languide/?get=users"));
+                var result = await response.Content.ReadAsStringAsync();
+                var json_object = JObject.Parse(result);
+                var json_array = JArray.Parse(json_object["data"].ToString());
+                foreach (var user in json_array)
+                {
+                    User usr = new User();
+                    usr.id_user = user["id_user"].ToString();
+                    usr.email = user["email"].ToString();
+                    usr.create_time = user["create_time"].ToString();
+                    users.Add(usr);
+                }
+            }
+            userListView.ItemsSource = users;
         }
     }
 }
