0bf74fa12190c07cf9ef988c383632c4cb05d614
Andrija Gradečak <andrija.gradecak@student.uniri.hr>
2022-07-05 12:28:10 +0200

    dodani modeli

--- LanGuide/LanGuide/Models/Result.cs
+++ LanGuide/LanGuide/Models/Result.cs
@@ -6,6 +6,15 @@ namespace LanGuide.Models
 {
     public class Result
     {
-
+        public string id_user { get; set; }
+        public string email { get; set; }
+        public string create_time { get; set; }
+        public string id_exercise { get; set; }
+        public string result_percent { get; set; }
+        public string result_score { get; set; }
+        public string result_max { get; set; }
+        public string skill { get; set; }
+        public string language { get; set; }
+        public string result_date { get; set; }
     }
 }
